a53dfd643d144fc5292d740db03441197899d9f4
samuelsurbakti <sammysurbakti@gmail.com>
2025-07-28 19:13:55 +0700

    delete online style in vertical layout

--- resources/views/ui/layouts/vertical/navbar.blade.php
+++ resources/views/ui/layouts/vertical/navbar.blade.php
@@ -361,16 +361,16 @@
             <!-- User -->
             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                 <a class="nav-link dropdown-toggle hide-arrow p-0" href="javascript:void(0);" data-bs-toggle="dropdown">
-                    <div class="avatar avatar-online">
+                    <div class="avatar">
                         <img src="/src/img/user/{{ auth()->user()->avatar }}" alt class="rounded-circle" />
                     </div>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-end">
                     <li>
-                        <a class="dropdown-item" href="pages-account-settings-account.html">
+                        <a class="dropdown-item" disabled>
                             <div class="d-flex">
                                 <div class="flex-shrink-0 me-3">
-                                    <div class="avatar avatar-online">
+                                    <div class="avatar">
                                         <img src="/src/img/user/{{ auth()->user()->avatar }}" alt class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </div>
